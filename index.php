f56215732328713881b13dfc96d3f83548cbc5e3
ladykakki <greta_calamari@hotmail.it>
2022-06-07 14:51:33 +0200

    prodotti generati

--- index.php
+++ index.php
@@ -41,3 +41,39 @@ $items = [
 ]
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+                <?php foreach($items as $key => $value){ 
+                            $items = new Products($value['name'],$value['brand'],$value['price']);
+                            
+                    ?>         
+            <div class="card col ms-3" style="width: 18rem;">
+                <!-- <img src="..." class="card-img-top" alt="..."> -->
+                <div class="card-body">
+                    <h5 class="card-title"> <?php echo $items->name ?> </h5>
+                    <p class="card-text"> <?php echo $items->brand ?> </p>
+                    <p class="card-text"> <?php echo $items->price ?> </p>
+
+                </div>
+                
+                
+            </div>
+            <?php } ?>
+            
+    </div>
+</div>
+</body>
+</html>
